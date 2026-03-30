018bca8bdb80e39cbbf0f126f5d621241732b4a8
Daniel Baumann <143436969+danielbaumannbr@users.noreply.github.com>
2026-03-30 14:25:26 -0300

    30031425

--- layout/src/app/pages/home/home.ts
+++ layout/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -10,15 +12,15 @@ export class Home {
   requisicao=[
     {
       tecnologia:'HTML',
-      descricao:'Estrutura da página'
+      descricao:'O HTML (HyperText Markup Language ou Linguagem de Marcação de Hipertexto) é a linguagem padrão utilizada para estruturar e organizar o conteúdo de páginas na web. Ele funciona como o "esqueleto" de um site, definindo onde ficam textos, imagens, links e outros elementos.'
     },
     {
       tecnologia:'CSS',
-      descricao:'Estilização da página'
+      descricao:'Se o HTML é o esqueleto, o CSS (Cascading Style Sheets ou Folhas de Estilo em Cascata) é a "roupa" e a maquiagem da sua página. Ele é usado para definir o design, as cores, as fontes e o layout dos elementos que você criou com HTML'
     },
     {
       tecnologia:'JavaScript',
-      descricao:'Lógica da página'
+      descricao:'Se o HTML é o esqueleto e o CSS é a roupa, o JavaScript (JS) é o cérebro. Ele é a linguagem de programação que traz vida, interatividade e dinamismo para as páginas da web.'
     }
   ];
 }
